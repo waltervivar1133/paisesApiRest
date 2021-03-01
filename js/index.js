18beaa654715d5027e4142bf2c1610a52642dc4a
walter <waltervivar1133@gmail.com>
2021-03-01 18:14:36 -0500

    FEAT: logica Ãpara mostrar api

---
+++ js/index.js
@@ -0,0 +1,45 @@
+const banderas = document.getElementById('banderas')
+
+document.addEventListener("DOMContentLoaded", e => {
+    fetchData()
+})
+
+const fetchData = async () => {
+    try {
+        const res = await fetch('https://restcountries.eu/rest/v2/lang/es')
+        const data = await res.json()
+        banderillas(data)
+        formularioCliente(data)
+        filtros(data)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const banderillas = data => {
+    let elementos = ''
+    data.forEach(item => {
+        elementos += `
+        <article class="card">
+            <img src="${item.flag}" alt="" class="img-fluid">
+            <div class="card-content">
+                <h3>${item.name}</h3>
+                <p>
+                    <b>Population: </b>
+                    ${item.population}
+                </p>
+                <p>
+                    <b>Capital: </b>
+                    ${item.capital}
+                </p>
+                <p>
+                    <b>Región: </b>
+                    ${item.region}
+                </p>
+               
+            </div>
+        </article>
+        `
+    });
+    banderas.innerHTML = elementos
+}
